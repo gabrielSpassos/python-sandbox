587693a264484a9515ef8d821448af6c9387ce85
gabriel passos <gabriel.passos@agibank.com.br>
2019-11-14 14:06:39 -0300

    :sparkles: add basics operators

---
+++ basics/algebraic-operators.py
@@ -0,0 +1,11 @@
+'''
+multi
+line
+coments
+'''
+print(2 + 2) # soma
+print(2 - 2) # subtracao
+print(2 * 9) # multiplicacao
+print(2 / 2) # divisao
+print(2 ** 3) #exponencialcao
+print(10 % 3) #modulo
