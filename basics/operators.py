a8a14983a243ab3d4874494c773c13f6d9cb2d75
gabriel passos <gabriel.passos@agibank.com.br>
2019-11-14 14:38:21 -0300

    :truck: rename file

---
+++ basics/operators.py
@@ -0,0 +1,17 @@
+'''
+multi
+line
+coments
+'''
+print(2 + 2) # soma
+print(2 - 2) # subtracao
+print(2 * 9) # multiplicacao
+print(2 / 2) # divisao
+print(2 ** 3) #exponencialcao
+print(10 % 3) #modulo
+print(1 == 1) # comparacao igual
+print(2 != 1) # comparacao diferente
+print(2 > 1) # comparacao maior
+print(2 >= 2) # comparacao maior ou igual
+print(1 < 2) # comparacao menor
+print(2 <= 2) # comparacao menor ou igual
