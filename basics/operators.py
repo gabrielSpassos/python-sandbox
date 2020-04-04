2384445fa2076294e8300bb86e6ad1a741d75871
gabriel passos <gabriel.passos@agibank.com.br>
2020-04-04 15:14:23 -0300

    refact: refactor basics

--- basics/operators.py
+++ basics/operators.py
@@ -3,20 +3,29 @@ multi
 line
 coments
 '''
-print(2 + 2) # soma
-print(2 - 2) # subtracao
-print(2 * 9) # multiplicacao
-print(2 / 2) # divisao
-print(2 ** 3) #exponencialcao
-print(10 % 3) #modulo
-print(1 == 1) # comparacao igual
-print(2 != 1) # comparacao diferente
-print(2 > 1) # comparacao maior
-print(2 >= 2) # comparacao maior ou igual
-print(1 < 2) # comparacao menor
-print(2 <= 2) # comparacao menor ou igual
+
+print(2 + 2)   # soma
+print(2 - 2)   # subtracao
+print(2 * 9)   # multiplicacao
+print(2 / 2)   # divisao
+print(2 ** 3)  # exponencialcao
+print(10 % 3)  # modulo
+print(1 == 1)  # comparacao igual
+print(2 != 1)  # comparacao diferente
+print(2 > 1)   # comparacao maior
+print(2 >= 2)  # comparacao maior ou igual
+print(1 < 2)   # comparacao menor
+print(2 <= 2)  # comparacao menor ou igual
 
 first_name = "Gabriel"
 last_name = "Passos"
 
-print(first_name  + " " + last_name)
+print(first_name + " " + last_name)
+
+list = [0, "ola", True, 9.99]
+
+for i in list:
+    print(i)
+
+for i in range(10, 20, 2):
+    print(i)
