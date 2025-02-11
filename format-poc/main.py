8f3e0dc1a4b1fb5e2da1e81203aa16422d1e1163
Gabriel Passos <gabrielsantos45725@gmail.com>
2025-02-11 13:10:07 -0800

    feat: add uuid equality,string poc

--- format-poc/main.py
+++ format-poc/main.py
@@ -5,6 +5,7 @@ id = uuid.uuid4()
 test1 = "test-{}".format(id)
 test2 = "test-{}".format(str(id))
 
-if (test1 == test2) {
+if (test1 == test2):
     print("Both strings are equal")
-}
+else:
+    print("Not equals")
