3ed6e6c5bc4f5282f7168b70476d38e55154bd44
Gabriel Passos <gabrielsantos45725@gmail.com>
2025-03-14 12:47:36 -0700

    refact: refactor pandas poc

--- pandas-poc/src/main.py
+++ pandas-poc/src/main.py
@@ -1,6 +1,24 @@
 import pandas as pd
+import re
+
+
+def clean_title(title):
+    return re.sub('\(\d+\)$', '', title).strip()
+
+
+def get_year_on_title(title):
+    match = re.search('\(\d+\)$', title)
+    if match:
+        return match.group(0).replace("(", "").replace(")", "")
+
+    return None
+
 
 movies_df = pd.read_csv("./resources/movies.csv")
+
+movies_df["year"] = movies_df["title"].apply(get_year_on_title)
+movies_df["title"] = movies_df["title"].apply(clean_title)
+
 ratings_df = pd.read_csv("./resources/ratings.csv")
 
 merged = pd.merge(movies_df, ratings_df, left_on='movieId', right_on='movieId', how='left')
@@ -8,9 +26,10 @@ print(merged.head())
 
 # Count how many times each movieId is present in the merged DataFrame
 merged_with_count = merged.groupby("movieId").size().reset_index(name='count').sort_values(by='count', ascending=False)
-print(merged_with_count)
+print(merged_with_count.head())
 
 final_df = pd.merge(merged, merged_with_count, left_on='movieId', right_on='movieId', how='left')
+print(final_df.head())
 
 # Create movie_title_input_profiles without the count
 movie_title_input_profiles = final_df.groupby("movieId").agg({
@@ -19,5 +38,4 @@ movie_title_input_profiles = final_df.groupby("movieId").agg({
     "rating": "mean",
     "count": "size"
 }).reset_index()
-print(movie_title_input_profiles)
-
+print(movie_title_input_profiles.head())
