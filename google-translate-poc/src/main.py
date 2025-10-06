72e2546c9280791e5e63592819490e6488166883
gabrielSpassos <gabrielsantos45725@gmail.com>
2025-10-06 09:55:15 -0300

    refact google translate api poc

--- google-translate-poc/src/main.py
+++ google-translate-poc/src/main.py
@@ -1,17 +1,6 @@
 import asyncio
 from googletrans import Translator
 
-# # Create a Translator object
-# translator = Translator()
-
-# # Translate text from English to Spanish
-# translated_text = translator.translate('Hello world', dest='es').text
-# print(f"Translated text (English to Spanish): {translated_text}")
-
-# # Translate text with automatic source language detection
-# translated_text_auto = translator.translate('Bonjour le monde', dest='en').text
-# print(f"Translated text (Auto-detect to English): {translated_text_auto}")
-
 async def translate_text():
     async with Translator() as translator:
         result = await translator.translate('안녕하세요.')
@@ -20,5 +9,10 @@ async def translate_text():
         print(result)
         result = await translator.translate('Hello', src='en', dest='pt')
         print(result)
+
+        # Translate list
+        translations = await translator.translate(['The quick brown fox', 'jumps over', 'the lazy dog'], dest='pt')
+        for translation in translations:
+            print(translation.origin, ' -> ', translation.text)
 
 asyncio.run(translate_text())
