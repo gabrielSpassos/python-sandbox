3bbc3df27fc6642de9cf988bf316a2c68a3223db
gabrielSpassos <gabrielsantos45725@gmail.com>
2025-11-25 09:46:25 -0300

    feat: update hours cli

--- cli-hours-tracker-poc/src/hours/main.py
+++ cli-hours-tracker-poc/src/hours/main.py
@@ -1,13 +1,15 @@
+import calendar
+from datetime import date
 import click
 import hours.datasource as datasource
 
 LOGO = click.style(r"""
-  ██████╗   ██████╗ 
-  ██╔══██╗  ██╔══██╗
-  ██║  ██║  ██████╔╝
-  ██║  ██║  ██╔══██╗
-  ██████╔╝  ██████╔╝
-  ╚═════╝   ╚═════╝ 
+██╗  ██╗ ██████╗ ██╗   ██╗██████╗ ███████╗
+██║  ██║██╔═══██╗██║   ██║██╔══██╗██╔════╝
+███████║██║   ██║██║   ██║██████╔╝█████╗  
+██╔══██║██║   ██║██║   ██║██╔══██╗██╔══╝  
+██║  ██║╚██████╔╝╚██████╔╝██║  ██║███████╗
+╚═╝  ╚═╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚══════╝
 """, fg="bright_cyan")
 
 
@@ -17,6 +19,7 @@ def cli(ctx):
     if ctx.invoked_subcommand is None:
         click.echo(LOGO)
         welcome_display()
+        click.echo(ctx.get_help())
 
 
 def welcome_display():
@@ -43,6 +46,42 @@ def collect_user_data(name, last_name, contract_hours):
         "contract_hours": contract_hours
     }
     datasource.update_datasource(data)
+
+
+@click.command()
+def get_hours_per_day():
+    datasource_data = datasource.get_or_create_datasource()
+
+    if "contract_hours" not in datasource_data:
+        collect_user_data.main(args=[], standalone_mode=False)
+    
+    contract_hours = datasource_data["contract_hours"]
+    working_days = working_days_in_current_month()
+    hours_per_day = contract_hours / working_days
+    click.echo(
+        f"You need to work {hours_per_day:.2f} hours per day during {working_days} working days "
+        f"to meet your contract of {contract_hours} hours this month."
+    )
+
+
+
+def working_days_in_current_month():
+    today = date.today()
+    year, month = today.year, today.month
+    
+    month_calendar = calendar.monthcalendar(year, month)
+    
+    working_days = sum(
+        1
+        for week in month_calendar
+        for day in week[:5]
+        if day != 0
+    )
+    
+    return working_days
+
+
+cli.add_command(get_hours_per_day)
 
 
 if __name__ == "__main__":
